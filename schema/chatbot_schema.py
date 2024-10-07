ea0279e5aefd5ed1292b1b0e7e6968fdf6785de0
Ehtsham <mr_ehtsham@yahoo.com>
2024-10-07 22:02:51 +0500

    Updated Code
    Add all Urls for data Scrap

--- schema/chatbot_schema.py
+++ schema/chatbot_schema.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import List
 class ChatbotRequest(BaseModel):
-    urls: List[str] = ["https://solar-studios.gitbook.io/solar-studios",
+    urls: List[str] = ["https://solar-studios.gitbook.io/solar-studios",                  
                        "https://solar-studios.gitbook.io/solar-studios/introduction-welcome-to-solar-studios/solar-dex",
                        "https://solar-studios.gitbook.io/solar-studios/introduction-welcome-to-solar-studios/solar-dominion",
                        "https://solar-studios.gitbook.io/solar-studios/introduction-welcome-to-solar-studios/solar-manga",
